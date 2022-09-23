0c06d798b16b9867be7bd4c288ca8a7bfabf9b94
Abi Noval Fauzi <bbhpowpow@gmail.com>
2022-09-23 14:32:56 +0700

    add: loading indicator

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import { AppContext } from '@/contexts/app-context'
+import Router from 'next/router'
 import { useState } from 'react'
 import '../styles/globals.css'
+import NProgress from 'nprogress' //nprogress module
+import 'nprogress/nprogress.css' //styles of nprogress
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }) {
 	const [user, setUser] = useState({})
