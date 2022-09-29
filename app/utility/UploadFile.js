4a8dbb7234ea4350cc6c773382678d32e402c6e7
FallenZx <anjashse@gmail.com>
2022-09-29 13:25:41 +0700

    fix: directory not found upload

--- app/utility/UploadFile.js
+++ app/utility/UploadFile.js
@@ -3,6 +3,10 @@ const { cwd } = require('process')
 const randomstring = require('randomstring')
 
 const store = (file, path) => {
+    if (!fs.existsSync(cwd() + path)) {
+        fs.mkdirSync(cwd() + path, { recursive: true })
+    }
+    
     const fileName =
         randomstring.generate() + Date.now() + '.' + file.mimetype.split('/')[1]
     const upload = cwd() + path + '/' + fileName
